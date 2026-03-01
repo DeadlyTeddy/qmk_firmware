e879b16570e06e3e718059146ba79127a055eaba
DeadlyTeddy <DeadlyTeddy@users.noreply.github.com>
2026-03-01 15:34:25 -0600

    Add bootloader combo and test keymap for matrix debugging
    - Add QK_BOOT combo: press 1+2+3 (left) + B+C+D (right) to enter bootloader
    - Update keymap with test characters to identify matrix positions
    - Fix keyboard.json formatting (spacing alignment)
    - Working on fixing matrix column mapping for bottom row and thumb clusters
    
    Current issues being debugged:
    - Right bottom row outputs 45678 instead of 12345 (column offset)
    - Left thumb cluster outputs wrong characters (matrix mismatch)
    - Right thumb cluster partially correct
    
    Physical layout documented in ~/Documents/Obsidian/the_vault/keeb/physical-layout-reference.md

--- keyboards/handwired/tractyl_manuform/4x7_right_deadlyteddy/keymaps/default/keymap.c
+++ keyboards/handwired/tractyl_manuform/4x7_right_deadlyteddy/keymaps/default/keymap.c
@@ -18,9 +18,13 @@
 
 const uint16_t PROGMEM combo1[] = {KC_A, KC_R, KC_S, KC_T, COMBO_END};
 const uint16_t PROGMEM combo2[] = {KC_LSFT, KC_S, KC_T, COMBO_END};
+const uint16_t PROGMEM bootloader_combo1[] = {KC_1, KC_2, KC_3, COMBO_END};
+const uint16_t PROGMEM bootloader_combo2[] = {KC_B, KC_C, KC_D, COMBO_END};
 combo_t key_combos[] = {
     COMBO(combo1, KC_F5),
     COMBO(combo2, LGUI(LCTL(KC_0))),
+    COMBO(bootloader_combo1, QK_BOOT),
+    COMBO(bootloader_combo2, QK_BOOT),
 };
 uint16_t COMBO_LEN = sizeof(key_combos) / sizeof(key_combos[0]);
 
@@ -36,33 +40,32 @@ enum custom_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_COLEMAK_DH] = LAYOUT_4x7_right(
-    KC_ESC ,KC_DEL , KC_Q  , KC_W  , KC_F  , KC_P  , KC_B  ,                                 KC_J  , KC_L  , KC_U  , KC_Y  ,KC_SCLN,KC_MINS,_______,
-    KC_F22 ,KC_BSPC, KC_A  , KC_R  , KC_S  , KC_T  , KC_G  ,                                 KC_M  , KC_N  , KC_E  , KC_I  , KC_O  ,KC_QUOT,_______,
-    KC_F23 ,KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_D  , KC_V  ,                                 KC_K  , KC_H  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLS,_______,
-    KC_F24 ,_______,KC_LWIN,KC_LBRC,KC_RBRC,                                                                KC_PLUS, KC_EQL,_______,_______,_______,
-                                            KC_LSFT,KC_LCTL,                            KC_SPC,
-                                                    LOWER  , KC_TAB,                   KC_DEL,
-                                                    KC_BSPC,KC_LWIN,          KC_LGUI, KC_ENT
+    KC_1  , KC_2  , KC_3  , KC_4  , KC_5  , KC_6  , KC_7  ,                                 KC_8  , KC_9  , KC_0  , KC_A  , KC_B  , KC_C  , KC_D  ,
+    KC_E  , KC_F  , KC_G  , KC_H  , KC_I  , KC_J  , KC_K  ,                                 KC_L  , KC_M  , KC_N  , KC_O  , KC_P  , KC_Q  , KC_R  ,
+    KC_S  , KC_T  , KC_U  , KC_V  , KC_W  , KC_X  , KC_Y  ,                                 KC_Z  , KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
+    KC_SCLN, KC_QUOT, KC_COMM, KC_DOT, KC_SLSH,                                                          KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,
+                                            KC_6  , KC_7  ,                                         KC_8  ,
+                                                    KC_9  , KC_0  ,                   KC_MINS,
+                                                    KC_EQL, KC_LBRC,          KC_RBRC, KC_BSLS
   ),
 
   [_LOWER] = LAYOUT_4x7_right(
     _______,_______,_______,_______,_______,_______,KC_LBRC,                                KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,_______,
     _______,_______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                                KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,_______,
     _______,_______,_______,_______,_______,_______,_______,                                _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,_______,
-    _______,_______,_______,_______,KC_PSCR,                                                                _______, KC_P0 ,_______,_______,_______,
-                                            _______,_______,                          _______,
-                                                    _______,_______,                  _______,
-                                                    _______,_______,          _______,_______
-
+    _______,_______,_______,_______,KC_PSCR,                                                                _______,_______,_______,_______,_______,
+                                            _______,_______,                                KC_P0 ,
+                                                    _______,_______,                _______,
+                                                    _______,_______,        _______,_______
   ),
 
   [_RAISE] = LAYOUT_4x7_right(
     _______,_______,_______,_______,_______,_______,KC_LBRC,                                KC_RBRC,_______,KC_NUM,KC_INS ,KC_SCRL, KC_MUTE,_______,
     _______,_______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                                KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,_______,
     _______,_______,_______,_______,_______,_______,_______,                                _______,_______,_______,_______,_______,KC_VOLD,_______,
-    _______,_______,_______,_______,_______,                                                                KC_EQL ,_______,_______,_______,_______,
-                                            _______,_______,                          _______,
-                                                    _______,_______,                  _______,
-                                                    _______,_______,          _______,_______
+    _______,_______,_______,_______,_______,                                                                _______,_______,_______,_______,_______,
+                                            _______,_______,                                KC_EQL ,
+                                                    _______,_______,                _______,
+                                                    _______,_______,        _______,_______
   ),
 };
